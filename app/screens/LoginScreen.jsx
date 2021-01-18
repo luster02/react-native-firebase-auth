03ba504597750dfd46c2bdf57833a8f69edb5e74
luster02 <bombolti_02@hotmail.com>
2021-01-18 14:08:04 -0600

    [ADD]: firebase auth

--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -1,55 +1,102 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions, TextInput, TouchableNativeFeedback, ScrollView } from 'react-native';
-import { Icon } from 'react-native-elements'
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Dimensions,
+    TextInput,
+    TouchableNativeFeedback,
+    ScrollView,
+    ActivityIndicator,
+    Alert,
+    SafeAreaView
+} from 'react-native';
+import { Icon, Button } from 'react-native-elements'
+import { useLogin } from '../hooks/useFirebase'
 
 const { height } = Dimensions.get('window');
 
 export default function Register({ navigation }) {
+    const [Email, setEmail] = useState('')
+    const [Password, setPassword] = useState('')
+    const [exec, { loading, error }] = useLogin()
+
+    const toForgot = () => navigation.navigate('Forgot')
+
+    function send() {
+        if (!Email || !Password) {
+            Alert.alert(
+                'Error',
+                'All fields are required',
+                [
+                    { text: "OK", onPress: () => null }
+                ]
+            )
+        } else {
+            exec({
+                email: Email,
+                password: Password
+            })
+        }
+    }
+
     return (
-        <ScrollView style={styles.mainView}>
-            <View style={styles.backView}>
-                <Icon
-                    raised
-                    name='arrow-left'
-                    type='font-awesome'
-                    onPress={navigation.goBack}
-                />
-            </View>
-            <View style={styles.buttonsView}>
+        <SafeAreaView style={styles.droidSafeArea}>
+            <ScrollView style={styles.mainView} keyboardShouldPersistTaps="handled">
+                <View style={styles.backView}>
+                    <Icon
+                        raised
+                        name='arrow-left'
+                        type='font-awesome'
+                        onPress={navigation.goBack}
+                    />
+                </View>
+                <View style={styles.buttonsView}>
 
 
-                <TextInput
-                    placeholder="Email"
-                    placeholderTextColor="black"
-                    style={styles.textInput}
-                />
+                    <TextInput
+                        placeholder="Email"
+                        placeholderTextColor="black"
+                        style={styles.textInput}
+                        onChangeText={text => setEmail(text)}
+                        value={Email || ''}
+                    />
 
-                <TextInput
-                    placeholder="Password"
-                    placeholderTextColor="black"
-                    style={styles.textInput}
-                    secureTextEntry={true}
-                />
+                    <TextInput
+                        placeholder="Password"
+                        placeholderTextColor="black"
+                        style={styles.textInput}
+                        secureTextEntry={true}
+                        onChangeText={text => setPassword(text)}
+                        value={Password || ''}
+                    />
 
 
-                <TouchableNativeFeedback>
-                    <View style={styles.button}>
-                        <Text style={{ ...styles.buttonText }}>
-                            SIGN IN
+                    <TouchableNativeFeedback disabled={loading} onPress={send}>
+                        <View style={loading ? styles.disabledBtn : styles.button}>
+                            <Text style={loading ? styles.disabledBtnText : { ...styles.buttonText }}>
+                                SIGN IN
                         </Text>
-                    </View>
-                </TouchableNativeFeedback>
+                        </View>
+                    </TouchableNativeFeedback>
 
-                <TouchableNativeFeedback >
-                    <View style={{ ...styles.button, backgroundColor: '#2E71DC' }}>
-                        <Text style={{ ...styles.buttonText, color: 'white' }}>
-                            SIGN IN WITH FACEBOOK
-                        </Text>
+                    <View style={styles.forgotViewStyle}>
+                        <Button
+                            title="forgot password"
+                            type="clear"
+                            titleStyle={{ color: "#000" }}
+                            onPress={toForgot}
+                        />
                     </View>
-                </TouchableNativeFeedback>
 
-            </View>
-        </ScrollView>
+                    {loading && <ActivityIndicator size="large" color="#000" />}
+
+
+                    {error && <Text style={styles.errorText}>Email or password incorrect</Text>}
+
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
@@ -58,6 +105,10 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    droidSafeArea: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? 25 : 0
     },
     backView: {
         position: 'absolute',
@@ -99,5 +150,35 @@ const styles = StyleSheet.create({
         paddingLeft: 10,
         marginVertical: 5,
         borderColor: `rgba(0,0,0,0.4)`
+    },
+    disabledBtn: {
+        height: 60,
+        marginHorizontal: 20,
+        borderRadius: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 5,
+        borderColor: `rgba(0,0,0,0.4)`,
+        borderWidth: 1,
+        shadowOffset: { width: 5, height: 5 },
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        backgroundColor: "#A9A9A9",
+    },
+    disabledBtnText: {
+        fontSize: 20,
+        fontWeight: '900',
+        color: "#fff"
+    },
+    errorText: {
+        textAlign: 'center',
+        color: 'red',
+        fontWeight: '300'
+    },
+    forgotViewStyle: {
+        width: "100%",
+        display: 'flex',
+        justifyContent: 'center',
+        paddingHorizontal: 50
     }
 });
--- app/screens/RegisterScreen.jsx
+++ app/screens/RegisterScreen.jsx
@@ -1,63 +1,103 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions, TextInput, TouchableNativeFeedback, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Dimensions,
+    TextInput,
+    TouchableNativeFeedback,
+    ScrollView,
+    ActivityIndicator,
+    Alert,
+    SafeAreaView
+} from 'react-native';
 import { Icon } from 'react-native-elements'
+import { useRegister } from '../hooks/useFirebase'
 
 const { height } = Dimensions.get('window');
 
 export default function Register({ navigation }) {
+    const [Email, setEmail] = useState('')
+    const [Password, setPassword] = useState('')
+    const [Password2, setPassword2] = useState('')
+    const [exec, { loading, error }] = useRegister()
+
+    if (error) console.log(error)
+
+    function send() {
+        if (Password !== Password2) {
+            Alert.alert(
+                'Error',
+                'Passwords do not match',
+                [
+                    { text: "OK", onPress: () => null }
+                ]
+            )
+        } else {
+            exec({
+                email: Email,
+                password: Password
+            })
+        }
+    }
 
     return (
-        <ScrollView style={styles.mainView}>
-            <View style={styles.backView}>
-                <Icon
-                    raised
-                    name='arrow-left'
-                    type='font-awesome'
-                    onPress={navigation.goBack}
-                />
-            </View>
+        <SafeAreaView style={styles.droidSafeArea}>
+            <ScrollView style={styles.mainView} keyboardShouldPersistTaps="handled">
+                <View style={styles.backView}>
+                    <Icon
+                        raised
+                        name='arrow-left'
+                        type='font-awesome'
+                        onPress={navigation.goBack}
+                    />
+                </View>
 
-            <View style={styles.buttonsView}>
+                <View style={styles.buttonsView}>
 
-                <TextInput
-                    placeholder="Email"
-                    placeholderTextColor="black"
-                    style={styles.textInput}
-                />
+                    <TextInput
+                        placeholder="Email"
+                        placeholderTextColor="black"
+                        style={styles.textInput}
+                        onChangeText={text => setEmail(text)}
+                        value={Email || ''}
+                    />
 
-                <TextInput
-                    placeholder="Password"
-                    placeholderTextColor="black"
-                    style={styles.textInput}
-                    secureTextEntry={true}
-                />
+                    <TextInput
+                        placeholder="Password"
+                        placeholderTextColor="black"
+                        style={styles.textInput}
+                        secureTextEntry={true}
+                        onChangeText={text => setPassword(text)}
+                        value={Password || ''}
+                    />
 
-                <TextInput
-                    placeholder="Confirm password"
-                    placeholderTextColor="black"
-                    style={styles.textInput}
-                    secureTextEntry={true}
-                />
+                    <TextInput
+                        placeholder="Confirm password"
+                        placeholderTextColor="black"
+                        style={styles.textInput}
+                        secureTextEntry={true}
+                        onChangeText={text => setPassword2(text)}
+                        value={Password2 || ''}
+                    />
 
 
-                <TouchableNativeFeedback>
-                    <View style={styles.button}>
-                        <Text style={{ ...styles.buttonText }}>
-                            SIGN UP
+                    <TouchableNativeFeedback disabled={loading} onPress={send}>
+                        <View style={loading ? styles.disabledBtn : styles.button}>
+                            <Text style={loading ? styles.disabledBtnText : { ...styles.buttonText }}>
+                                SIGN UP
                         </Text>
-                    </View>
-                </TouchableNativeFeedback>
+                        </View>
+                    </TouchableNativeFeedback>
 
-                <TouchableNativeFeedback >
-                    <View style={{ ...styles.button, backgroundColor: '#2E71DC' }}>
-                        <Text style={{ ...styles.buttonText, color: 'white' }}>
-                            SIGN UP WITH FACEBOOK
-                        </Text>
-                    </View>
-                </TouchableNativeFeedback>
+                    {loading && <ActivityIndicator size="large" color="#000" />}
 
-            </View>
-        </ScrollView>
+
+                    {error && <Text style={styles.errorText}>Interal server error, try later</Text>}
+
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
@@ -66,6 +106,10 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    droidSafeArea: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? 25 : 0
     },
     backView: {
         position: 'absolute',
@@ -107,5 +151,29 @@ const styles = StyleSheet.create({
         paddingLeft: 10,
         marginVertical: 5,
         borderColor: `rgba(0,0,0,0.4)`
+    },
+    disabledBtn: {
+        height: 60,
+        marginHorizontal: 20,
+        borderRadius: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 5,
+        borderColor: `rgba(0,0,0,0.4)`,
+        borderWidth: 1,
+        shadowOffset: { width: 5, height: 5 },
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        backgroundColor: "#A9A9A9",
+    },
+    disabledBtnText: {
+        fontSize: 20,
+        fontWeight: '900',
+        color: "#fff"
+    },
+    errorText: {
+        textAlign: 'center',
+        color: 'red',
+        fontWeight: '300'
     }
 });
