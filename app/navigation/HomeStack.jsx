bbcc4a594eea0ec3563d4695c79c0641102e9b9e
luster02 <bombolti_02@hotmail.com>
2021-01-18 01:19:57 -0600

    [ADD]: home stack

---
+++ app/navigation/HomeStack.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import HomeScreen from '../screens/HomeScreen'
+const Stack = createStackNavigator()
+
+export function HomeNavigation() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    headerShown: false
+                }}
+            />
+        </Stack.Navigator>
+    )
+}
