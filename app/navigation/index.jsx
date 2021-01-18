1793890186da8718c1387454242882ea81c11794
luster02 <bombolti_02@hotmail.com>
2021-01-18 01:20:08 -0600

    [ADD]: main navigation

---
+++ app/navigation/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import { ActivityIndicator } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import * as firebase from 'firebase'
+
+//stacks
+import { HomeNavigation } from './HomeStack'
+import { AuthNavigation } from './AuthRegister'
+
+
+const Drawer = createDrawerNavigator();
+
+export default function Navigation() {
+    const [isAuth, setIsAuth] = useState(false)
+    const [Loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        (() => {
+            setLoading(true)
+            firebase.auth().onAuthStateChanged((user) => {
+                user ? setIsAuth(true) : setIsAuth(false)
+                setLoading(false)
+            })
+        })()
+    }, [])
+
+    return (
+        Loading
+            ? <ActivityIndicator size="large" color="000" />
+            : <NavigationContainer>
+                {isAuth
+                    ? <Drawer.Navigator initialRouteName="Home">
+                        <Drawer.Screen
+                            name="Home"
+                            component={HomeNavigation}
+                        />
+                    </Drawer.Navigator>
+                    : <AuthNavigation />
+                }
+            </NavigationContainer>
+    )
+}
