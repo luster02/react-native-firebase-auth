0f7f6eb915ae1173fa4e64b0bbbb151f72f537db
luster02 <bombolti_02@hotmail.com>
2021-01-18 01:18:47 -0600

    [ADD]: global styles

---
+++ app/styles/main.styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+export const mainStyles = StyleSheet.create({
+    center: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
